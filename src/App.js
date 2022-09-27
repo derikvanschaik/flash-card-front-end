1c1194c5f0e3b21581500761cbd05f3efc6e1888
Derik van Schaik <derikvanschaik@gmail.com>
2022-09-27 16:00:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactCardFlip from 'react-card-flip';
+import {Card, Button, Container} from 'react-bootstrap';
+import {useState} from 'react';
 
 function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={ {height: '100vh'} }>
+      <div style={{height: '80%'}} className='d-flex flex-column align-items-center justify-content-center'>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div className='text-center'>
+          <Card className='py-4 px-2' >
+            <Card.Body>Do turtles have feelings?</Card.Body>
+          </Card>
+        </div>
+        <div className='text-center'>
+          <Card className='py-4 px-2'>
+            <Card.Body>No, turtles don't have feelings</Card.Body>
+          </Card>
+        </div>
+      </ReactCardFlip>
+      <Button className='my-2' onClick={()=> setIsFlipped(!isFlipped)}>Flip</Button>
+      </div>
+    </Container>
   );
 }
 
