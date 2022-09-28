9a208e2fe0716350434082db0890d27e927714c5
Derik van Schaik <derikvanschaik@gmail.com>
2022-09-28 14:57:58 -0700

    add add and edit card functionality:

--- src/App.js
+++ src/App.js
@@ -14,7 +14,15 @@ function App() {
     {question: 'Question 3', answer: 'Answer3'}
   ]
   const [flashCards, setFlashCards] = useState(cards);
-
+  const handleAdd = () =>{
+    setCurCardIdx(flashCards.length);
+    setFlashCards([...flashCards, {answer: 'Click Edit to change answer.', question: 'click edit to change question.'}])
+  }
+  const handleDelete = (idx) =>{
+    const newFlashCards = flashCards.filter((_, i) => i !== idx);
+    setFlashCards(newFlashCards);
+    setCurCardIdx( newFlashCards.length === 0? -1 :(curCardIdx) % newFlashCards.length);
+  }
   const handleNextCard = () =>{
     setIsEditing(false);
     setCurCardIdx( (curCardIdx + 1) % flashCards.length);
@@ -28,16 +36,31 @@ function App() {
   return (
     <Container style={ {height: '100vh'} }>
       <div style={{height: '80%'}} className='d-flex flex-column align-items-center justify-content-center'>
-        <p className='h4'>({ (curCardIdx + 1) }/{flashCards.length})</p>
-        <FlipCard 
-          isFlipped={isFlipped} 
-          isEditing={isEditing} 
-          question={flashCards[curCardIdx].question} 
-          answer={flashCards[curCardIdx].answer} 
-          handleSubmit={handleSubmit} />
-        <Button className='my-2' onClick={()=> setIsFlipped(!isFlipped)}>Flip</Button>
-        <Button className='my-2' onClick={handleNextCard} disabled={isFlipped === true}>Next</Button>
-        <Button className='my-2' onClick={() => setIsEditing(!isEditing)}>{isEditing? 'Cancel' : 'Edit'}</Button>
+        <Button className='my-1' onClick={handleAdd}>Add card</Button>
+        {
+          flashCards.length > 0 &&
+          <>
+            <div className='d-flex flex-row align-items-center justify-content-between'>
+              <p className='h4'>({ (curCardIdx + 1) }/{flashCards.length})</p>
+            </div>
+            <FlipCard 
+              isFlipped={isFlipped} 
+              isEditing={isEditing} 
+              question={flashCards[curCardIdx].question} 
+              answer={flashCards[curCardIdx].answer} 
+              handleSubmit={handleSubmit} />
+            <div className='flex-row align-items-center justify-content-around'>
+              <Button className='m-2' onClick={() => setIsEditing(!isEditing)}>{isEditing? 'Cancel' : 'Edit'}</Button>
+              <Button className='m-2' onClick={() => handleDelete(curCardIdx)}>Delete</Button>
+              <Button className='m-2' onClick={()=> setIsFlipped(!isFlipped)}>Flip</Button>
+              <Button className='m-2' onClick={handleNextCard} disabled={isFlipped === true}>Next</Button>
+            </div>
+          </>
+        }
+        {
+          flashCards.length === 0 &&
+          <p className='alert alert-danger'>No flash cards. Please add one.</p>
+        }
       </div>
     </Container>
   );
